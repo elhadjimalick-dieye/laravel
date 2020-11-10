acb22ac49d20e3dc461f57100a7307a3849ae03a
elhadji malick dieye <ndieyene@gmail.com>
2020-11-10 04:21:23 +0000

    make it better

--- resources/views/achats/create.blade.php
+++ resources/views/achats/create.blade.php
@@ -21,7 +21,7 @@
 
 @if (count($errors) > 0)
 <div class="alert alert-danger">
-  <strong>Whoops!</strong> There were some problems with your input.<br><br>
+  <strong>Error!</strong> There were some problems with your input.<br><br>
   <ul>
     @foreach ($errors->all() as $error)
     <li>{{ $error }}</li>
