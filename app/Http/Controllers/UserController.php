4e53af3674ab0ec3d49fa0ee2d58764fb3f0e236
elhadji malick dieye <ndieyene@gmail.com>
2020-11-10 05:06:50 +0000

    make it better

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -88,7 +88,7 @@ class UserController extends Controller
         $input['password'] = Hash::make($input['password']);
 
         $user = User::create($input);
-        $user->assignRole($request->input('roles'));
+        //$user->assignRole($request->input('roles'));
         return redirect()->route('users.index',compact('user'))
                         ->with('success','User created successfully');
     }
